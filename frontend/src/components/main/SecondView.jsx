4217889a59fa57843b56f2b0eb67bc467270a9ba
soyoon26 <barcardi26@gmail.com>
2024-11-02 21:08:18 +0900

    feat: 비로그인시 등록에서 로그인창으로 이동

--- frontend/src/components/main/SecondView.jsx
+++ frontend/src/components/main/SecondView.jsx
@@ -8,16 +8,23 @@ import Carousel from "./Carousel";
 
 export default function SecondView() {
   const images = [register, dailycheck, follow, possible, tips];
+  const links = [
+    "/register",
+    "/dailycheck",
+    "https://www.instagram.com/peachpicker_official?igsh=MTBpc282aTBjcWRvYQ%3D%3D&utm_source=qr",
+    "/drawings",
+    "/tips",
+  ];
   console.log("두번째");
   return (
     <div className="flex flex-col items-center justify-center h-full space-y-6">
       <section className="w-5/6 max-w-full mb-4">
         <div className="pl-10 mb-2 text-lg font-bold">실시간 랭킹</div>
-        <Carousel images={images} />
+        <Carousel images={images} links={links} />
       </section>
       <section className="w-5/6 max-w-full mb-4">
         <div className="pl-10 mb-2 text-lg font-bold">진행중인 이벤트</div>
-        <Carousel images={images} />
+        <Carousel images={images} links={links} />
       </section>
     </div>
   );
